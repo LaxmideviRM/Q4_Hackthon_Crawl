7c535eac3eec7dd0ebfa05cbafae2804bff2cdc0
LaxmideviRM <laxmidevi.mokhi@careerbuilder.com>
2016-10-13 17:30:26 +0530

    new changes

--- test.js
+++ test.js
@@ -5,26 +5,50 @@ var options = {
     }
 };
  
+var clicks = [];
 var client = webdriverio.remote(options).init()
 
-    .url('http://www.klier-karriere.de/jobs')
+    .url('https://apply.king.com/vacancy/find/results/')
     .getTitle().then(function(title) {
         console.log('Title was: ' + title);
+
     })
-    .elements('a')
-    .then(function(links) {
-        for (var i=0; i<links.value.length; i++) {
-            console.log('Clicking link...');
-            // console.log(links.value.length)
-            var link = links.value[i].ELEMENT;
-            // console.log(links[i].getText());
-            client.elementIdClick(link).then(function(result) {
-                console.log('Link clicked!');
-                // console.log(link.getText());
-                // console.log(result);
-            });
-        }
+    .click(".rowContainerHolder1").pause(1000).then(function(res) {
+        console.log("PPPPPPPPPPPPPPPPPPPPP")
+        console.log(res);
     })
+    
+        .getURL().then(function(url){
+            console.log("its here");
+            console.log(url);
+        })
+        client.getCurrentTabId().then(function(tabid) {
+            console.log(tabid);
+            console.log("****************")
+        })
+        client.getHTML(res, function(html) {
+            console.log("its inside page");
+            console.log(html);
+        })
+        client.getText('h1').then(function(job_title){
+            console.log("job title is " + job_title);
+        })
+    
+    // .elements('a').then(function(res) {
+    //     console.log("its coming here")
+    //     console.log(res)
+    //     res.value.forEach(function(ele) {
+    //         // console.log(ele);
+    //         clicks.push(client.elementIdClick(elem.ELEMENT)
+    //                 // .pause(5000)
+    //                 // .back()
+    //                 // .pause(2000)
+
+    //         );
+    //         console.log(clicks);
+    //     });
+    //     return q.all(clicks);
+    // })
     // .click(".jsopenwindow").then(function (argument) {
     //     console.log("PPPPPPPPPPPPPPPP");
     // })
@@ -38,3 +62,4 @@ var client = webdriverio.remote(options).init()
    
 
     .end();
+
